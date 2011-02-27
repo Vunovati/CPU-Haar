ebf5b1e6a38fa0329b69711889b1ad416f6605e4
Vladimir Adamić <vladimir.adamic@gmail.com>
2011-02-27 23:48:05 +0100

    Pocisceno, boja jos ne radi

--- CpuHaarCV.cpp
+++ CpuHaarCV.cpp
@@ -33,7 +33,6 @@ float *haarStep(float *cpu_image, int rows, int columns)
 	return( filtered_image ); 
 }
 
-
 float *haarStepBGR(float *cpu_image, int rows, int columns, int boja)
 {
 	float* filtered_image = new float[rows * columns * 4];
@@ -111,190 +110,73 @@ char *float2CVdata(float *image, int nPixels, int channels)
 
 int main(int argc, char *argv[])
 {
-	IplImage* img = cvLoadImage("testnaslika.jpg", CV_LOAD_IMAGE_UNCHANGED); 
-  int height,width,step,channels,rows,columns,boja;
-  uchar *data;
-  int i,j,k,x,y,xx;
-  float L12, L34, H12, H34;
-  
-
-  // get the image data
-  height    = img->height;
-  width     = img->width;
-  step      = img->widthStep;
-  channels  = img->nChannels;
-  /*data      = (uchar *)img->imageData;*/
-  printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
-
-  float* cpu_image = new float[width * height * 4];
-
-  float* filtered_image = new float[width * height * 4];
-  float* filtered_blue = new float[width * height * 4];
-  float* filtered_green = new float[width * height * 4];
-  float* filtered_red = new float[width * height * 4];
-
-  //float* filtered_blue = new float[width * height * 4];
-  //float* filtered_green = new float[width * height * 4];
-  //float* filtered_red = new float[width * height * 4];
-  //float* HH = new float[width * height];
-  //float* LH = new float[width * height];
-  //float* HL = new float[width * height];
-  //float* LL = new float[width * height];
-  char* buff = new char[(width) * (height) * 4];
-  IplImage* out_image = cvCreateImage(cvSize(width, height), img->depth, channels);
-
-  // create a window
-  cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
-  cvMoveWindow("mainWin", 100, 100);
-
-  // invert the image
-  //for(i=0;i<height;i++) for(j=0;j<width;j++) for(k=0;k<channels;k++)
-  //  data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
-
- //  za crno bijelu sliku
- // for (int i = 0; i < width * height; i++)
-	//{
-	//	cpu_image[i] = (unsigned char)img->imageData[i] / 255.f; // pretvori svaki element iz chara u float (vrijednosti 0..1)
-	//}
-  cpu_image = CV2float(img);
-
- // for (int i = 0; i < (width * height); i++)
-	//{
-	//	cpu_blue[i] = (unsigned char)img->imageData[i * 4 + 0] / 255.f; // |B|G|R|prazno|B|G|R|prazno|B|G|R|prazno|
-	//	cpu_green[i] = (unsigned char)img->imageData[i * 4 + 1] / 255.f;
-	//	cpu_red[i] = (unsigned char)img->imageData[i * 4 + 2] / 255.f;
-	//}
-
-	
-	// OBRADA
-
-		//for (x =0;x<rows/2;x++)
-		//{
-		//	for (y = 0;y<columns/2;y++)
-		//	{
-		//	xx = 2*(x+rows*y);
-		//
-		//	L12 = (cpu_image[xx]+cpu_image[xx+1]);
-		//	L34 = (cpu_image[xx+rows]+cpu_image[xx+rows+1]);
-		//	H12 = (cpu_image[xx]-cpu_image[xx+1]);
-		//	H34 = (cpu_image[xx+rows]-cpu_image[xx+rows+1]);
-		//
-		//	HH[x+rows/2*y]= (H12-H34)/2;
-		//	LL[x+rows/2*y]= (L12+L34)/2;
-		//	LH[x+rows/2*y]= (L12-L34)/2;
-		//	HL[x+rows/2*y]= (H12+H34)/2;
-
-		//	}
-		//}
-	// za crno bijelo
-		
-	
-	//for (x=0; x<rows/2; x++)
-	//	{
-	//		for(y=0; y<columns/2; y++)
-	//		{
-	//			xx = 2*(x + rows*y); // [0][0] svakog kvadratica (svaki drugi po retcima i stupcima)
-	//			L12 = (cpu_image[xx] + cpu_image[xx+1]); // kvadratic[0][0] + kvadratic[1][0]
-	//			L34 = (cpu_image[xx + rows] + cpu_image[xx + rows + 1]); // kvadratic[0][1] + kvadratic[1][1]
-	//			H12 = (cpu_image[xx] - cpu_image[xx+1]); // kvadratic[0][0] - kvadratic[1][0]
-	//			H34 = (cpu_image[xx + rows] - cpu_image[xx + rows + 1]); // kvadratic[0][1] - kvadratic[1][1]
-
-	//			filtered_image[x + rows*y] = (L12 + L34)/4; // gornji lijevi kvadrant
-	//			filtered_image[(x + rows/2) + rows*y ] = (L12 - L34)/4; // donji lijevi kvadrant
-	//			filtered_image[x + rows/2 * columns + rows*y] = (H12 + H34)/4; // gornji desni kvadrant
-	//			filtered_image[(x + rows/2) + rows/2 * columns + rows*y] = (H12 - H34)/4; // donji desni kvadrant
-	//		}
-	//	}
+	//IplImage* img = cvLoadImage("testnaslika.jpg", CV_LOAD_IMAGE_UNCHANGED); 
+	IplImage* img = cvLoadImage("testnaslika.jpg", CV_LOAD_IMAGE_GRAYSCALE);   
+	int height,width,step,channels, i;
 
 
-		
-	//for (boja=0; boja<channels; boja++)
-	//{
-	//	// implementiraj kao funkciju
-	//	for (x=0; x<rows/2; x++)
-	//	{
-	//		for(y=0; y<columns/2; y++)
-	//		{
-	//			xx = channels * (2*(x + rows*y)) + boja; // [0][0] indeks svakog kvadratica (svaki drugi po retcima i stupcima)
-	//			
-	//			L12 = (cpu_image[xx] + cpu_image[xx+1]); // kvadratic[0][0] + kvadratic[1][0]
-	//			L34 = (cpu_image[xx + rows] + cpu_image[xx + rows + 1]); // kvadratic[0][1] + kvadratic[1][1]
-	//			H12 = (cpu_image[xx] - cpu_image[xx+1]); // kvadratic[0][0] - kvadratic[1][0]
-	//			H34 = (cpu_image[xx + rows] - cpu_image[xx + rows + 1]); // kvadratic[0][1] - kvadratic[1][1]
+	// get the image data
+	height    = img->height;
+	width     = img->width;
+	step      = img->widthStep;
+	channels  = img->nChannels;
+	/*data      = (uchar *)img->imageData;*/
+	printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
 
-	//			filtered_image[channels * (x + rows*y) + boja] = (L12 + L34)/4; // gornji lijevi kvadrant
-	//			filtered_image[channels * ((x + rows/2) + rows*y) + boja] = (L12 - L34)/4; // donji lijevi kvadrant
-	//			filtered_image[channels * (x + rows/2 * columns + rows*y) + boja] = (H12 + H34)/4; // gornji desni kvadrant
-	//			filtered_image[channels * ((x + rows/2) + rows/2 * columns + rows*y) + boja] = (H12 - H34)/4; // donji desni kvadrant
-	//		}
-	//	}
-	//}
+	float* cpu_image = new float[width * height * 4];
+
+	float* filtered_image = new float[width * height * 4];
+	float* filtered_blue = new float[width * height * 4];
+	float* filtered_green = new float[width * height * 4];
+	float* filtered_red = new float[width * height * 4];
 
 
-	// prebacivanje podataka nazad u format za openCV
+	char* buff = new char[(width) * (height) * 4];
+	IplImage* out_image = cvCreateImage(cvSize(width, height), img->depth, channels);
 
-	//for (int i = 0; i < ((width) * (height)); i++)
-	//{
-	//	buff[i * channels + 0] = (char)floor(filtered_image[i * 4 + 0] * 255.f);
-	//	buff[i * channels + 1] = (char)floor(filtered_image[i * 4 + 1] * 255.f);
-	//	buff[i * channels + 2] = (char)floor(filtered_image[i * 4 + 2] * 255.f);
-	//}
+	cpu_image = CV2float(img);
 
-	//for (int i = 0; i < (width * height) ; i++)
-	//{
-	//	buff[i * 4 + 0] = (char)floor(filtered_blue[i] * 255.f);
-	//	buff[i * 4 + 1] = (char)floor(filtered_green[i] * 255.f);
-	//	buff[i * 4 + 2] = (char)floor(filtered_red[i] * 255.f);
-	//	buff[i * 4 + 3] = 0;
-	//}
 
-  if (channels = 1)
-  {
-	  filtered_image = haarStep(cpu_image, height, width); // pozivamo funkciju za haar
-  }
-  else 
-  {
-	  filtered_blue = haarStepBGR(cpu_image, height, width, 0); // haar na plavom kanalu
-	  filtered_green = haarStepBGR(cpu_image, height, width, 1); // haar na zelenom kanalu
-	  filtered_red = haarStepBGR(cpu_image, height, width, 2); // haar na crvenom kanalu
-
-	for (i = 0; i< height * width; i++)
+	if (channels = 1)
 	{
-		filtered_image[i * 3 + 0] = filtered_blue[i];
-		filtered_image[i * 3 + 1] = filtered_green[i];
-		filtered_image[i * 3 + 2] = filtered_red[i];
+		filtered_image = haarStep(cpu_image, height, width); // pozivamo funkciju za haar
 	}
-  }
-	 
+	else 
+	{
+		filtered_blue = haarStepBGR(cpu_image, height, width, 0); // haar na plavom kanalu
+		filtered_green = haarStepBGR(cpu_image, height, width, 1); // haar na zelenom kanalu
+		filtered_red = haarStepBGR(cpu_image, height, width, 2); // haar na crvenom kanalu
+
+		for (i = 0; i< height * width; i++)
+		{
+			filtered_image[i * 3 + 0] = filtered_blue[i];
+			filtered_image[i * 3 + 1] = filtered_green[i];
+			filtered_image[i * 3 + 2] = filtered_red[i];
+		}
+	}
+
 	buff = float2CVdata(filtered_image, width*height, channels);
 
 	out_image->imageData = buff; // napunimo podatke za sliku podatcima iz buffera
 
 
-  /*cvFlip(img,img,-1);*/
+	// show the image
+	cvShowImage("Haar", out_image );
+	cvShowImage("Original", img );
 
-  // show the image
-  cvShowImage("Haar", out_image );
-  cvShowImage("Original", img );
+	// wait for a key
+	cvWaitKey(0);
 
-  // wait for a key
-  cvWaitKey(0);
+	// oslobodi memoriju za buffere
+	delete[] cpu_image;
+	delete[] buff;
+	delete[] filtered_image;
+	delete[] filtered_blue;
+	delete[] filtered_green;
+	delete[] filtered_red;
 
-  // oslobodi memoriju za buffere
-  delete[] cpu_image;
-  delete[] buff;
-  delete[] filtered_image;
-  delete[] filtered_blue;
-  delete[] filtered_green;
-  delete[] filtered_red;
-  //delete[] HH;
-  //delete[] LH;
-  //delete[] HL;
-  //delete[] LL;
-
-  // release the image
-  cvReleaseImage(&img );
-  cvReleaseImage(&out_image);
-  
-  return 0;
+	// release the image
+	cvReleaseImage(&img );
+	cvReleaseImage(&out_image);
+	return 0;
 }
